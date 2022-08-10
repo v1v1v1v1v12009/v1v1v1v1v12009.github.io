86ff949779a58a4b18eb28af9bb18467e0944e06
v1v1v1v1v12009 <109622326+v1v1v1v1v12009@users.noreply.github.com>
2022-08-10 18:53:29 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ text = document.getElementById("texti");
 div = document.getElementById("divi");
 
 function check(){
-    if (text.value == 2009){
+    if (text.value == 20000000000000000000000000000000000000000000000000000000000009){
         alert("סיסמא נכונה!");
         div.style.visibility = "visible";
     }else{
